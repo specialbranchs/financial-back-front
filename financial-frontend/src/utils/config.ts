32913d82bff9609d14e9295c22fd1fca4a0cba8e
specialbranchs <111952627+specialbranchs@users.noreply.github.com>
2023-11-13 14:22:10 +0600

    first

--- financial-frontend/src/utils/config.ts
+++ financial-frontend/src/utils/config.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL='http://127.0.0.1:8000'
+export const BACKEND_URL='https://fislam.pythonanywhere.com'
 export const BACKEND_BASE=`${BACKEND_URL}/api`
 export const PASSWORD_MIN_LENGTH=6
 
