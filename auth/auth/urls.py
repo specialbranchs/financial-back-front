80e27d5f143000f800fb5e41e17759a475240e06
specialbranchs <111952627+specialbranchs@users.noreply.github.com>
2023-12-24 14:54:58 +0600

    production build

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -19,11 +19,12 @@ from django.urls import path,include
 
 from django.conf.urls.static import static
 from django.conf import settings
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('backend.urls')),
-    path('',include('frontend.urls'))
+    # path('dammy/',include('frontend.urls')),
+    path('',views.index,name='index'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
