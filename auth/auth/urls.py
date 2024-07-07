1c9bc453680e0c455569f94d8a1c6362efa9e78b
specialbranchs <specialbranch64@gmail.com>
2024-07-07 14:58:57 +0600

    htaccess

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -26,6 +26,5 @@ urlpatterns = [
     # path('dammy/',include('frontend.urls')),
     path('',views.index,name='index'),
 ]
-urlpatterns += static(settings.STATIC_URL, 
-   document_root=settings.STATIC_ROOT)
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
