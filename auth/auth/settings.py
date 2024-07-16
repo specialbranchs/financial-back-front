cbf2a9d2fbb05e9afd20ebe878a3aad7db65a6fe
specialbranchs <specialbranch64@gmail.com>
2024-07-16 13:47:48 +0600

    whitenoise

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'finance_root',
-        'USER':'root',
-        'PASSWORD':'sb@db#$2024',
-        'HOST':'127.0.0.1',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'finance_root',
+    #     'USER':'root',
+    #     'PASSWORD':'sb@db#$2024',
+    #     'HOST':'127.0.0.1',
+    #     'PORT':'3306'
+    # }
   
 }
 
@@ -133,15 +133,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'  #ok
+MEDIA_URL = '/media/'
 
-MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'build', 'static')
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') #ok
+if DEBUG:
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+else:
+
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
