418fb3d4c3bd5822daadb2485cc64b15c6fdf87d
Faridul <fislam@Fariduls-MacBook-Pro.local>
2024-07-30 18:04:47 +0600

    dev

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework',
     'backend',
-    'frontend',
     "corsheaders"
 
 
@@ -81,11 +80,16 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+if DEBUG:
+  DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+  
+    }
+else:
+   DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'finance_root',
@@ -94,8 +98,7 @@ DATABASES = {
         'HOST':'127.0.0.1',
         'PORT':'3306'
     }
-  
-}
+   }
 
 
 # Password validation
@@ -145,18 +148,6 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# STATIC_URL = '/static/'  #ok
-
-# MEDIA_URL = '/media/'
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'build', 'static')
-# ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static') #ok
-
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- auth/frontend/templatetags/medal.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from backend.models import PodokName
-register = template.Library()
-
-@register.inclusion_tag('html/tags/medal.html')
-def podok_name (pk:None):
-    podok=PodokName.objects.get(pk=pk)
-    return {'podok':podok}
