b4fe92fe04067d71b73f8733277cc58ce60cc2e9
specialbranchs <specialbranch64@gmail.com>
2024-07-30 13:33:50 +0600

    first

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -133,17 +133,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = '/static/'  #ok
+# MEDIA_URL = '/media/'
+
+
+# if DEBUG:
+
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# else:
+
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'  #ok
+
 MEDIA_URL = '/media/'
 
-
-if DEBUG:
-
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-else:
-
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'build', 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static') #ok
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
